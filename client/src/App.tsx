52e724e8d8f8fa779f12228c0066fb3620e18380
ScottHahn1 <scotthahn2210@gmail.com>
2026-04-22 21:21:51 +0200

    feat: add base app layout with sidebar and main container

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,18 @@
+import Sidebar from "./components/Sidebar"
+import Applications from "./pages/Applications"
+import Dashboard from "./pages/Dashboard"
+
 function App() {
 
   return (
-    <>
-    </>
+    <div className="flex">
+      <Sidebar />
+
+      <main className="flex-1 min-w-0 p-5">
+        <Dashboard />
+        <Applications />
+      </main>
+    </div>
   )
 }
 
