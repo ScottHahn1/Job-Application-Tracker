c85f10ee52c21809086b7360d8de120bbd6a3be0
ScottHahn1 <scotthahn2210@gmail.com>
2026-05-10 16:21:19 +0200

    feat: add /api/users API route to server

--- server/server.ts
+++ server/server.ts
@@ -1,21 +1,26 @@
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
+import usersRouter from "./routes/users";
 
 dotenv.config();
 
 const app = express();
+
 const port = process.env.PORT ?? 8888;
 
 const frontendUrl = process.env.NODE_ENV === "production" 
 ? "" 
 : "http://localhost:5173";
 
-app.use(express.json());
 app.use(cors({
-    origin: frontendUrl,
-    methods: ["GET", "POST", "DELETE", "PATCH", "PUT"],
-    credentials: true
+  origin: frontendUrl,
+  methods: ["GET", "POST", "DELETE", "PATCH", "PUT"],
+  credentials: true
 }));
+
+app.use(express.json());
+
+app.use("/api/users", usersRouter);
 
 app.listen(port, () => console.log(`Server is running on port: ${port}`));
