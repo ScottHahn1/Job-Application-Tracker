205666646cc3fed3bba108fff35bec725dd71154
ScottHahn1 <scotthahn2210@gmail.com>
2026-05-26 12:21:01 +0200

    feat: add cookie-parser middleware for handling cookies

--- server/server.ts
+++ server/server.ts
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import usersRouter from "./routes/users";
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -20,6 +21,7 @@ app.use(cors({
 }));
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/users", usersRouter);
 
