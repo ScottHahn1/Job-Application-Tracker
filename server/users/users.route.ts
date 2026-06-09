5be843164f9afcfaf2cd7289a42d60544216a59b
ScottHahn1 <scotthahn2210@gmail.com>
2026-06-09 13:44:21 +0200

    refactor: move login/logout routes to users router

--- server/users/users.route.ts
+++ server/users/users.route.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { register } from "./users.controller";
+import { login, logout, register } from "./users.controller";
 
 const usersRouter = Router();
 
 usersRouter.post("/register", register);
+usersRouter.post("/login", login);
+usersRouter.post("/logout", logout);
 
 export default usersRouter;
