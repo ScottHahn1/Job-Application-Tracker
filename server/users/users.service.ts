e704037da548f7fcc57688c194ad59b1e889f2b2
ScottHahn1 <scotthahn2210@gmail.com>
2026-06-09 13:45:52 +0200

    refactor: move user login/logout business logic to service

--- server/users/users.service.ts
+++ server/users/users.service.ts
@@ -1,6 +1,7 @@
-import { Request, Response } from "express";
 import pool from "../config/database";
 import bcrypt from "bcrypt";
+import { RowDataPacket } from "mysql2";
+import jwt from "jsonwebtoken";
 
 const saltRounds = 10;
 
@@ -10,4 +11,39 @@ export const createUser = async (email: string, password: string) => {
   const hash = await bcrypt.hash(password, saltRounds);
   
   await pool.query(sql, [email, hash]);
+}
+
+export const loginUser = async (email: string, password: string) => {
+  const sql = "SELECT * FROM users WHERE email = ?";
+
+  const [result] = await pool.query<RowDataPacket[]>(sql, [email]);
+  const user = result[0];
+
+  if (!user) {
+    throw new Error("Invalid email");
+  }
+
+  const match = await bcrypt.compare(password, user.hashed_password);
+
+  if (!match) {
+    throw new Error("Incorrect password");
+  }
+
+  const accessToken = jwt.sign(
+    { userId: user.id },
+    process.env.ACCESS_TOKEN_SECRET as string,
+    { expiresIn: "15m" }
+  );
+
+  const refreshToken = jwt.sign(
+    { userId: user.id },
+    process.env.REFRESH_TOKEN_SECRET as string,
+    { expiresIn: "7d" }
+  );
+
+  return {
+    accessToken, 
+    refreshToken, 
+    userId: user.id
+  }
 }
