de7567a8449a108d294775cf607981fb3653e2cf
ScottHahn1 <scotthahn2210@gmail.com>
2026-06-17 21:52:34 +0200

    refactor: move refresh token route into users.route.ts

--- server/users/users.route.ts
+++ server/users/users.route.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { login, logout, register } from "./users.controller";
+import { login, logout, refreshAccessToken, register } from "./users.controller";
 
 const usersRouter = Router();
 
 usersRouter.post("/register", register);
 usersRouter.post("/login", login);
 usersRouter.post("/logout", logout);
+usersRouter.post("/refresh-token", refreshAccessToken);
 
 export default usersRouter;
