24ec0495c54a71cd4da62bdede3ba4fe1ea28bc2
ScottHahn1 <scotthahn2210@gmail.com>
2026-06-18 22:50:18 +0200

    refactor: move current user endpoint to users.route.ts

--- server/users/users.route.ts
+++ server/users/users.route.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
-import { login, logout, refreshAccessToken, register } from "./users.controller";
+import { getCurrentUser, login, logout, refreshAccessToken, register } from "./users.controller";
+import authenticateToken from "../middlewares/authToken";
 
 const usersRouter = Router();
 
@@ -7,5 +8,6 @@ usersRouter.post("/register", register);
 usersRouter.post("/login", login);
 usersRouter.post("/logout", logout);
 usersRouter.post("/refresh-token", refreshAccessToken);
+usersRouter.get("/me", authenticateToken, getCurrentUser);
 
 export default usersRouter;
