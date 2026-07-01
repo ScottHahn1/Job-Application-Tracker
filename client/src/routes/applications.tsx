a95e67636b123f8d1f7887f22b551cc8089d9a95
ScottHahn1 <scotthahn2210@gmail.com>
2026-07-01 10:20:16 +0200

    feat: integrate ApplicationForm into applications page

--- client/src/routes/applications.tsx
+++ client/src/routes/applications.tsx
@@ -1,18 +1,41 @@
 import { createFileRoute } from '@tanstack/react-router'
 import Kanban from '../components/Kanban'
+import { useState } from 'react';
+import ApplicationForm from '../components/ApplicationForm';
 
 export const Route = createFileRoute("/applications")({
   component: Applications,
 })
 
 function Applications() {
-   return (
+  const [showForm, setShowForm] = useState(false);
+  const [company, setCompany] = useState("");
+  const [jobTitle, setJobTitle] = useState("");
+  const [status, setStatus] = useState("");
+  const [date, setDate] = useState("");
+
+  return (
     <div className="p-2">
       <button 
         className="py-2 px-4 bg-orange-500 hover:bg-orange-600 cursor-pointer focus:outline-none focus:ring-2 focus:ring-orange-400 text-white rounded-md font-semibold"
+        onClick={() => setShowForm(true)}
       >
         Add Job
       </button>
+
+      { showForm && 
+        <ApplicationForm
+          company={company}
+          setCompany={setCompany}
+          jobTitle={jobTitle}
+          setJobTitle={setJobTitle}
+          status={status}
+          setStatus={setStatus}
+          date={date}
+          setDate={setDate}
+          setShowForm={setShowForm}
+         /> 
+      }
 
       <Kanban />
     </div>
