f4cb179c07ff084a1a0cad7bc49499c4efa80a20
ScottHahn1 <scotthahn2210@gmail.com>
2026-07-06 12:21:44 +0200

    feat: add API helper for creating job applications

---
+++ client/src/api/applications.ts
@@ -0,0 +1,25 @@
+interface PostVariables {
+  company: string;
+  jobTitle: string;
+  status: string;
+  date: string;
+}
+
+export const addApplication = async ({ company, jobTitle, status, date }: PostVariables) => {
+  const response = await fetch("http://localhost:8888/api/applications", {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ company, jobTitle, status, date})
+  })
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || "Something went wrong");
+  }
+
+  return data;
+}
