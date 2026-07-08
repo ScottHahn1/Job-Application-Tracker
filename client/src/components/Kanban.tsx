6b28a021074bf72d41a7bf57cd43c7a3d078b3d5
ScottHahn1 <scotthahn2210@gmail.com>
2026-07-08 16:03:27 +0200

    feat: fetch and display user applications

--- client/src/components/Kanban.tsx
+++ client/src/components/Kanban.tsx
@@ -1,49 +1,41 @@
-import Card, { type CardProps } from "./Card";
+import { useQuery } from "@tanstack/react-query"
+import { getApplications } from "../api/applications"
+import Card, { type Status } from "./Card";
 
-const statuses = ["Applied", "Interview", "Offer", "Rejected"];
+interface Applications {
+  company_name: string;
+  job_title: string;
+  status: Status;
+  dateApplied: string;
+}
 
 const Kanban = () => {
-  const data : CardProps[] = [
-    {
-      company: 'Google',
-      role: 'Software Engineer',
-      status: 'Applied',
-      dateApplied: '15/04/2026'
-    },
-    {
-      company: 'Nexora Technologies',
-      role: 'Full Stack Developer',
-      status: 'Interview',
-      dateApplied: '07/04/2026'
-    },
-    {
-      company: 'BluePixel Labs',
-      role: 'Front-End Developer',
-      status: 'Offer',
-      dateApplied: '03/03/2026'
-    },
-    {
-      company: 'BluePixel Labs',
-      role: 'Front-End Developer',
-      status: 'Offer',
-      dateApplied: '03/03/2026'
-    },
-  ]
+  const statuses = ["applied", "interview", "offer", "rejected"];
+
+  const { data: applications, isLoading, isError } = useQuery<Applications[]>({
+    queryKey: ["applications"],
+    queryFn: getApplications
+  })
 
   return (
     <div className="flex gap-6 mt-4 ">
       {
-        statuses.map(status => (
-          <div key={status} className="w-80  bg-gray-100 rounded-2xl p-4 flex flex-col gap-3">
+        statuses?.map(status => (
+          <div className="w-80  bg-gray-100 rounded-2xl p-4 flex flex-col gap-3">
             <h2 className="font-semibold text-lg text-gray-700 px-3">
               {status}
             </h2>
 
             {
-              data
-              .filter(item => item.status === status)
-              .map(item => (
-                <Card {...item} />
+              applications
+              ?.filter(application => application.status === status)
+              .map(application => (
+                <Card
+                  company={application.company_name}
+                  role={application.job_title}
+                  status={application.status}
+                  dateApplied={application.dateApplied}
+                />
               ))
             }
           </div>
