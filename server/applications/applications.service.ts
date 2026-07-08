f19f778027de29d8dc512d0bb354d326be739edd
ScottHahn1 <scotthahn2210@gmail.com>
2026-07-08 16:05:43 +0200

    feat: add service to retrieve user job applications

--- server/applications/applications.service.ts
+++ server/applications/applications.service.ts
@@ -11,3 +11,11 @@ export const createApplication = async (
 
   await pool.query(sql, [userId, company, jobTitle, status, date]);
 }
+
+export const getApplications = async (userId: number) => {
+  const sql = "SELECT * FROM applications WHERE user_id = ?";
+
+  const [rows] = await pool.query(sql, [userId]);
+  
+  return rows;
+}
