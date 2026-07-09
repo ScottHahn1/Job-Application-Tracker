f19f68144c549e9e74f97b06cc577cf8b1814abf
ScottHahn1 <scotthahn2210@gmail.com>
2026-07-09 11:05:51 +0200

    add controller for fetching user job applications

--- server/applications/applications.controller.ts
+++ server/applications/applications.controller.ts
@@ -1,6 +1,6 @@
 import { Response } from "express";
-import { createApplication as createApplicationService } from "./applications.service";
-import { CustomRequest } from "../middlewares/authToken";
+import { createApplication as createApplicationService, getApplications as getApplicationsService } from "./applications.service";
+import { CustomRequest } from "../middlewares/authToken"; 
 
 export const createApplication = async (req: CustomRequest, res: Response) => {
   const { company, jobTitle, status, date } = req.body;
@@ -13,6 +13,18 @@ export const createApplication = async (req: CustomRequest, res: Response) => {
   try {
     await createApplicationService(userId, company, jobTitle, status, date);
     return res.status(201).json({ message: "Application added" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+}
+
+export const getApplications = async (req: CustomRequest, res: Response) => {
+  const userId = req.user!.userId;
+
+  try {
+    const data = await getApplicationsService(Number(userId));
+    return res.status(200).json(data);
   } catch (error) {
     console.error(error);
     return res.status(500).json({ error: "Internal server error" });
