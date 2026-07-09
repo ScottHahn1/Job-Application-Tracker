067035ab31b34db8c2f78355a626b3a68713e10c
ScottHahn1 <scotthahn2210@gmail.com>
2026-07-09 11:04:40 +0200

    add authenticated GET route for job applications

--- server/applications/applications.route.ts
+++ server/applications/applications.route.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import authenticateToken from "../middlewares/authToken";
 import { createApplication } from "./applications.controller";
+import { getApplications } from "./applications.controller";
 
 const applicationsRouter = Router();
 
 applicationsRouter.post("/", authenticateToken, createApplication);
+applicationsRouter.get("/", authenticateToken, getApplications);
 
 export default applicationsRouter;
