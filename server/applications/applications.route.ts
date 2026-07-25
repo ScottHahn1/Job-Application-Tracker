a4eab2635c5bd128d6898f24487a6b01c6052b99
ScottHahn1 <scotthahn2210@gmail.com>
2026-07-25 13:04:01 +0200

    feat: add application statistics route

--- server/applications/applications.route.ts
+++ server/applications/applications.route.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import authenticateToken from "../middlewares/authToken";
 import { createApplication, deleteApplication } from "./applications.controller";
 import { getApplications } from "./applications.controller";
+import { totalApplications } from "./applications.controller";
 
 const applicationsRouter = Router();
 
 applicationsRouter.post("/", authenticateToken, createApplication);
 applicationsRouter.get("/", authenticateToken, getApplications);
 applicationsRouter.delete("/", authenticateToken, deleteApplication);
+applicationsRouter.get("/total", authenticateToken, totalApplications);
 
 export default applicationsRouter;
