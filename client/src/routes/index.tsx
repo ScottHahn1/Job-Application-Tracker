5c70675a2847b8384cb8954984d11cac2c1678ad
ScottHahn1 <scotthahn2210@gmail.com>
2026-07-27 13:41:54 +0200

    refactor: split dashboard into reusable application components

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,10 +1,40 @@
+import { useQuery } from "@tanstack/react-query"
 import { createFileRoute } from "@tanstack/react-router"
-import Dashboard from "../pages/Dashboard"
+import { getTotalApplications } from "../api/applications"
+import { useUserContext } from "../contexts/userContext";
+import TotalApplications from "../components/totalApplications";
+import RecentApplications from "../components/RecentApplications";
 
-export const Route = createFileRoute('/')({
-  component: Dashboard
+export interface TotalApplicationsData {
+  total: number;
+  applied: number;
+  interview: number;
+  offer: number;
+  rejected: number;
+}
+
+export const Route = createFileRoute("/")({
+  component: Home
 })
 
-// function Index () {
-//   return <Dashboard />
-// }
+function Home () {
+  const { user } = useUserContext();
+
+  const { data: total, isLoading, isError, error } = useQuery<TotalApplicationsData>({
+    queryKey: ["totalApplications"],
+    queryFn: () => getTotalApplications(user === null)
+  })
+
+  return (
+    <div className="p-4 flex flex-col gap-8 bg-gray-50">
+      <TotalApplications
+        isLoading={isLoading}
+        isError={isError}
+        error={error}
+        total={total}
+      />
+
+      <RecentApplications />
+    </div>
+  )
+}
