901a00dc25841f9489e546ee84885b23d3dfe237
ScottHahn1 <scotthahn2210@gmail.com>
2026-07-27 09:13:46 +0200

    feat: add formatDate utility

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -1,8 +1,8 @@
 import type { Status } from "../types/applications";
-import { formatStatus } from "../utils/applications";
+import { formatDate, formatStatus } from "../utils/applications";
 import DeleteApplication from "./DeleteApplication";
 
-interface CardProps {
+export interface CardProps {
   id: number
   company: string;
   role: string;
@@ -12,18 +12,22 @@ interface CardProps {
 
 const Card = ({ id, company, role, status, dateApplied }: CardProps) => {
   const statusColours = {
-    applied: 'bg-sky-100 text-sky-600',
-    interview: 'bg-orange-100 text-orange-600',
-    offer: 'bg-green-100 text-green-600',
-    rejected: 'bg-red-100 text-red-600',
+    applied: "bg-sky-100 text-sky-600",
+    interview: "bg-orange-100 text-orange-600",
+    offer: "bg-green-100 text-green-600",
+    rejected: "bg-red-100 text-red-600",
   }
 
   const statusColour = statusColours[status];
 
   return (
-    <div className="p-4 rounded-xl shadow-sm border border-gray-200 hover:shadow-md transition">
+    <div className="p-4 rounded-xl shadow-sm border flex flex-col gap-1 border-gray-200 hover:shadow-md transition">
       <div className="flex justify-between items-start">
-        <h3 className="font-semibold text-gray-900">
+        <h3 
+          className="font-semibold line-clamp-2 min-h-10 text-gray-900"
+          aria-label={role}
+          title={role}
+        >
           {role}
         </h3>
 
@@ -32,8 +36,8 @@ const Card = ({ id, company, role, status, dateApplied }: CardProps) => {
         </span>
       </div>
 
-      <p className="text-sm text-gray-500 mt-2">{company}</p>
-      <p className="text-xs text-gray-400 mt-1">{dateApplied.toString()}</p>
+      <p className="text-sm text-gray-500 mt-2 line-clamp-1">{company}</p>
+      <p className="text-xs text-gray-400 mt-1">Applied On: {formatDate(dateApplied)}</p>
 
       <DeleteApplication id={id} />
     </div>
--- client/src/utils/applications.ts
+++ client/src/utils/applications.ts
@@ -1,3 +1,11 @@
 export const formatStatus = (status: string) => {
   return status[0].toUpperCase() + status.slice(1);
 }
+
+export const formatDate = (date: string) => {
+  return new Date(date).toLocaleDateString("en-GB", {
+    day: "numeric",
+    month: "short",
+    year: "numeric",
+  });
+}
